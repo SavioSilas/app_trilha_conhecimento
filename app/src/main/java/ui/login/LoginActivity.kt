736a286fc5ff4e6b2ddf8a7d6e959472d1e39af4
SavioSilas <savio.saboia@icomp.ufam.edu.br>
2025-05-16 16:42:28 -0400

    feat: Inicial commit Android Studio project

---
+++ app/src/main/java/ui/login/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.ct.apptrilhavt.ui.login
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ct.apptrilhavt.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // iniciar fluxo de login Firebase
+    }
+}
---
+++ app/src/main/java/ui/profile/ProfileActivity.kt
@@ -0,0 +1,14 @@
+package com.ct.apptrilhavt.ui.profile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ct.apptrilhavt.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        // exibir e editar nome e senha
+    }
+}
